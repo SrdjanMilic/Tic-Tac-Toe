78d211a2a5fdb8d475dbfbcb223334257f3c1c58
Srdjan Milic <srdjanrmilic@gmail.com>
2020-07-27 19:08:59 +0200

    disabled dynamic apikey creation for new users

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -1,24 +1,44 @@
 <template>
   <div class="boards">
-    <CreateBoard />
     <ListBoards />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import CreateBoard from '@/components/CreateBoard.vue';
 import ListBoards from '@/components/ListBoards.vue';
+// import { mapActions, mapState } from 'vuex';
+// import axios from 'axios';
 
 export default {
   name: 'Home',
+
   components: {
-    CreateBoard,
     ListBoards
   }
+
+  // computed: {
+  //   ...mapState(['apiKey', 'server'])
+  // },
+
+  // // create apikey for every new user if doesn't have one
+  // created () {
+  //   if (!this.apiKey) {
+  //     axios({
+  //       method: 'POST',
+  //       url: `${this.server}/register_candidate`
+  //     }).then(response => {
+  //       this.setApiKey(response.data.apikey);
+  //     });
+  //   }
+  //   window.app = this;
+  // },
+
+  // methods: {
+  //   ...mapActions(['setApiKey'])
+  // },
 };
 </script>
 
 <style scoped>
-
 </style>
