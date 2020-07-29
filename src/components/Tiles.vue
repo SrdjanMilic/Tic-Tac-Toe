675caf529c1c24ae6e89c630b917bebe62549425
Srdjan Milic <srdjanrmilic@gmail.com>
2020-07-29 22:59:28 +0200

    set players number dynamiclly

--- src/components/Tiles.vue
+++ src/components/Tiles.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="board-wrapper">
       <div class="game-board">
-        <div id="box-0" class="box" @click="markedTile0"></div>
-        <div id="box-1" class="box" @click="markedTile1"></div>
-        <div id="box-2" class="box" @click="markedTile2"></div>
-        <div id="box-3" class="box" @click="markedTile3"></div>
-        <div id="box-4" class="box" @click="markedTile4"></div>
-        <div id="box-5" class="box" @click="markedTile5"></div>
-        <div id="box-6" class="box" @click="markedTile6"></div>
-        <div id="box-7" class="box" @click="markedTile7"></div>
-        <div id="box-8" class="box" @click="markedTile8"></div>
+        <div id="tile-0" class="tile" @click="markedTile0"></div>
+        <div id="tile-1" class="tile" @click="markedTile1"></div>
+        <div id="tile-2" class="tile" @click="markedTile2"></div>
+        <div id="tile-3" class="tile" @click="markedTile3"></div>
+        <div id="tile-4" class="tile" @click="markedTile4"></div>
+        <div id="tile-5" class="tile" @click="markedTile5"></div>
+        <div id="tile-6" class="tile" @click="markedTile6"></div>
+        <div id="tile-7" class="tile" @click="markedTile7"></div>
+        <div id="tile-8" class="tile" @click="markedTile8"></div>
       </div>
     </div>
 </template>
 
 <script>
 import io from 'socket.io-client';
+import { mapState } from 'vuex';
 
 export default {
   name: 'Tiles',
@@ -24,15 +25,23 @@ export default {
     return {
       playerId: this.$store.state.player.id,
       socket: io.connect('http://178.128.206.150:7000/?id=' + this.playerId),
+      player1: null,
+      player2: null,
       x: 'X',
       o: 'O'
     };
   },
 
+  computed: {
+    ...mapState(['board', 'boards'])
+  },
+
   methods: {
     markedTile0 () {
-      const box0 = document.getElementById('box-0');
-      if (box0) box0.innerText = this.x;
+      const tile0 = document.getElementById('tile-0');
+      if (tile0 && this.player1) tile0.innerText = this.x;
+      if (tile0 && this.player2) tile0.innerText = this.o;
+
       this.socket.on('marked', res => {
         this.socket.emit(
           console.log(res) // doesn't get any response
@@ -41,44 +50,59 @@ export default {
     },
 
     markedTile1 () {
-      const box1 = document.getElementById('box-1');
-      if (box1) box1.innerText = this.x;
+      const tile1 = document.getElementById('tile-1');
+      if (tile1 && this.player1) tile1.innerText = this.x;
+      if (tile1 && this.player2) tile1.innerText = this.o;
     },
 
     markedTile2 () {
-      const box2 = document.getElementById('box-2');
-      if (box2) box2.innerText = this.x;
+      const tile2 = document.getElementById('tile-2');
+      if (tile2 && this.player1) tile2.innerText = this.x;
+      if (tile2 && this.player2) tile2.innerText = this.o;
     },
 
     markedTile3 () {
-      const box3 = document.getElementById('box-3');
-      if (box3) box3.innerText = this.x;
+      const tile3 = document.getElementById('tile-3');
+      if (tile3 && this.player1) tile3.innerText = this.x;
+      if (tile3 && this.player2) tile3.innerText = this.o;
     },
 
     markedTile4 () {
-      const box4 = document.getElementById('box-4');
-      if (box4) box4.innerText = this.x;
+      const tile4 = document.getElementById('tile-4');
+      if (tile4 && this.player1) tile4.innerText = this.x;
+      if (tile4 && this.player2) tile4.innerText = this.o;
     },
 
     markedTile5 () {
-      const box5 = document.getElementById('box-5');
-      if (box5) box5.innerText = this.x;
+      const tile5 = document.getElementById('tile-5');
+      if (tile5 && this.player1) tile5.innerText = this.x;
+      if (tile5 && this.player2) tile5.innerText = this.o;
     },
 
     markedTile6 () {
-      const box6 = document.getElementById('box-6');
-      if (box6) box6.innerText = this.x;
+      const tile6 = document.getElementById('tile-6');
+      if (tile6 && this.player1) tile6.innerText = this.x;
+      if (tile6 && this.player2) tile6.innerText = this.o;
     },
 
     markedTile7 () {
-      const box7 = document.getElementById('box-7');
-      if (box7) box7.innerText = this.x;
+      const tile7 = document.getElementById('tile-7');
+      if (tile7 && this.player1) tile7.innerText = this.x;
+      if (tile7 && this.player2) tile7.innerText = this.o;
     },
 
     markedTile8 () {
-      const box8 = document.getElementById('box-8');
-      if (box8) box8.innerText = this.x;
+      const tile8 = document.getElementById('tile-8');
+      if (tile8 && this.player1) tile8.innerText = this.x;
+      if (tile8 && this.player2) tile8.innerText = this.o;
     }
+  },
+
+  mounted () {
+    this.boards.forEach(element => {
+      if (element.id === this.board.id && element.players === 1) this.player1 = 1;
+      if (element.id === this.board.id && element.players === 2) this.player2 = 2;
+    });
   }
 };
 </script>
@@ -94,7 +118,7 @@ export default {
     grid-template-columns: 130px 130px 130px;
 }
 
-.box {
+.tile {
   background-color: #dddddd;
   border: 1px solid #b9b9b9;
   display: flex;
@@ -104,7 +128,7 @@ export default {
   font-size: 100px;
 }
 
-.box:hover {
+.tile:hover {
   cursor: pointer;
 }
 </style>
